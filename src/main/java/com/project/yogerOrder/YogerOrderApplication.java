196bb27b9898ae63cda36d94da57efef481da95f
siwoo <k74415834@gmail.com>
2024-09-28 15:46:33 +0900

    init: init project

---
+++ src/main/java/com/project/yogerOrder/YogerOrderApplication.java
@@ -0,0 +1,13 @@
+package com.project.yogerOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogerOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogerOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/yogerOrder/YogerOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yogerOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogerOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
