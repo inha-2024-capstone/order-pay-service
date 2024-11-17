88b681214f3d7747f9427750356841e55258e100
siwoo <k74415834@gmail.com>
2024-11-18 04:26:31 +0900

    feat: health-check 구현

---
+++ src/main/java/com/project/yogerOrder/global/controller/CommonController.java
@@ -0,0 +1,13 @@
+package com.project.yogerOrder.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+
+    @GetMapping("/health-check")
+    public String hello() {
+        return "Yoger order Server is alive";
+    }
+}
