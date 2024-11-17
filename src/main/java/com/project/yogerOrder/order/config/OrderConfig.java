118ae83f1f5b2231fc1d425be6baeba43b966359
siwoo <k74415834@gmail.com>
2024-11-18 04:58:12 +0900

    feat: 주문 서비스 설정 구현

---
+++ src/main/java/com/project/yogerOrder/order/config/OrderConfig.java
@@ -0,0 +1,8 @@
+package com.project.yogerOrder.order.config;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "order")
+public record OrderConfig(@NotNull Integer timeLimit) {
+}
