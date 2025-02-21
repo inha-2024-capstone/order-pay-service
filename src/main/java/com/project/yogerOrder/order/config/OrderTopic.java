8965b4df6c3f66f464100a9ee0c68a24e28d358a
siwoo <k74415834@gmail.com>
2025-02-21 22:36:19 +0900

    feat: event type으로 topic get하기 구현

--- src/main/java/com/project/yogerOrder/order/config/OrderTopic.java
+++ src/main/java/com/project/yogerOrder/order/config/OrderTopic.java
@@ -1,8 +1,26 @@
 package com.project.yogerOrder.order.config;
 
+import com.project.yogerOrder.order.event.OrderEventType;
+
 public class OrderTopic {
     public static final String CREATED = "yoger.order.prd.created";
     public static final String COMPLETED = "yoger.order.prd.completed";
     public static final String CANCELED = "yoger.order.prd.canceled";
     public static final String ERRORED = "yoger.order.prd.errored";
+
+
+    public static String getTopicByEvent(OrderEventType orderEventType) {
+        switch (orderEventType) {
+            case CREATED:
+                return CREATED;
+            case COMPLETED:
+                return COMPLETED;
+            case CANCELED:
+                return CANCELED;
+            case ERRORED:
+                return ERRORED;
+            default:
+                throw new RuntimeException("Invalid event type");
+        }
+    }
 }
