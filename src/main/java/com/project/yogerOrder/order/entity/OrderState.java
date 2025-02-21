55aefc7e1bf55628486a7ee0e06f4a4339545727
siwoo <k74415834@gmail.com>
2025-02-22 01:36:06 +0900

    refactor: order canceled event 재고, 결제 확인 여부 추가

--- src/main/java/com/project/yogerOrder/order/entity/OrderState.java
+++ src/main/java/com/project/yogerOrder/order/entity/OrderState.java
@@ -8,4 +8,12 @@ public enum OrderState {
     public static List<OrderState> getPayableStates() {
         return List.of(CREATED, STOCK_CONFIRMED);
     }
+
+    public static Boolean isStockOccupied(OrderState state) {
+        return state == STOCK_CONFIRMED || state == COMPLETED;
+    }
+
+    public static Boolean isPaymentCompleted(OrderState state) {
+        return state == PAYMENT_COMPLETED || state == COMPLETED;
+    }
 }
