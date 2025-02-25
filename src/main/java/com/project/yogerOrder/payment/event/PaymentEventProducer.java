dc93239e0a853fd1ff6fc432dce40163b1c20554
siwoo <k74415834@gmail.com>
2025-02-25 16:15:57 +0900

    refactor: payment event producer의 switch case -> if else

--- src/main/java/com/project/yogerOrder/payment/event/PaymentEventProducer.java
+++ src/main/java/com/project/yogerOrder/payment/event/PaymentEventProducer.java
@@ -2,6 +2,7 @@ package com.project.yogerOrder.payment.event;
 
 
 import com.project.yogerOrder.payment.entity.PaymentEntity;
+import com.project.yogerOrder.payment.entity.PaymentState;
 import com.project.yogerOrder.payment.event.outbox.service.PaymentOutboxService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,24 +16,18 @@ public class PaymentEventProducer {
     private final PaymentOutboxService paymentOutboxService;
 
     public void sendEventByState(PaymentEntity paymentEntity) {
-        switch (paymentEntity.getState()) {
-            case PAID_END:
-                sendPaymentCompletedEvent(paymentEntity);
-                break;
-            case TEMPORARY_PAID:
-                //sendPaymentCompletedEvent(paymentEntity);
-                log.info("Temporary paid payment is ignored: {}", paymentEntity);
-                break;
-            case CANCELED:
-                sendPaymentCanceledEvent(paymentEntity);
-                break;
-            case ERROR:
-                sendPaymentCanceledEvent(paymentEntity);
-                sendPaymentErroredEvent(paymentEntity);
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown payment state: " + paymentEntity.getState());
+        if (paymentEntity.getState() == PaymentState.TEMPORARY_PAID) {
+            // temporary paid payment is ignored
+        } else if (paymentEntity.getState() == PaymentState.PAID_END) {
+            sendPaymentCompletedEvent(paymentEntity);
+        } else if (paymentEntity.getState() == PaymentState.CANCELED) {
+            sendPaymentCanceledEvent(paymentEntity);
+        } else if (paymentEntity.getState() == PaymentState.ERROR) {
+            sendPaymentCanceledEvent(paymentEntity);
+            sendPaymentErroredEvent(paymentEntity);
         }
+
+        throw new IllegalArgumentException("Invalid payment state: " + paymentEntity.getState());
     }
 
     private void sendPaymentCompletedEvent(PaymentEntity paymentEntity) {
