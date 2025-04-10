3562cfdd25fe80f485a1cad11b4a118ccda5420c
siwoo <k74415834@gmail.com>
2025-04-10 16:28:23 +0900

    feat: redisson client 설정 추가

--- src/main/java/com/project/yogerOrder/global/config/RedisConfig.java
+++ src/main/java/com/project/yogerOrder/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.project.yogerOrder.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,5 +29,15 @@ public class RedisConfig {
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		return redisTemplate;
+	}
+
+	@Bean
+	public RedissonClient redissonClient(RedisProperties properties) {
+		Config config = new Config();
+		config.useSingleServer()
+			.setAddress("redis://" + properties.getHost() + ":" + properties.getPort())
+			.setPassword(properties.getPassword());
+
+		return Redisson.create(config);
 	}
 }
