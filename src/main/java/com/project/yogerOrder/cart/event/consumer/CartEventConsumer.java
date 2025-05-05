f3db6cd027e8f275002d34dfdf880a90a5b79f36
seate <sa20207@naver.com>
2025-05-05 18:08:45 +0900

    feat: cart Event Consumer 구현

---
+++ src/main/java/com/project/yogerOrder/cart/event/consumer/CartEventConsumer.java
@@ -0,0 +1,34 @@
+package com.project.yogerOrder.cart.event.consumer;
+
+import java.util.Map;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+import com.project.yogerOrder.cart.dto.request.DeleteProductFromCartRequestDTO;
+import com.project.yogerOrder.cart.service.CartService;
+import com.project.yogerOrder.global.config.KafkaConfig;
+import com.project.yogerOrder.order.config.OrderTopic;
+import com.project.yogerOrder.order.event.OrderCompletedEvent;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CartEventConsumer {
+
+	private final CartService cartService;
+
+
+	@KafkaListener(topics = OrderTopic.COMPLETED, groupId = KafkaConfig.CART_GROUP,
+		containerFactory = KafkaConfig.KafkaConsumerConfig.ORDER_COMPLETED_FACTORY)
+	public void productDeductionCompleted(OrderCompletedEvent event, Acknowledgment acknowledgment) {
+		DeleteProductFromCartRequestDTO requestDTO = new DeleteProductFromCartRequestDTO(
+			event.getProducts().stream().map(Map.Entry::getKey).toList()
+		);
+		cartService.deleteItems(event.getUserId(), requestDTO);
+
+		acknowledgment.acknowledge();
+	}
+}
