c701406729d6c84c998c0e6165cbe756b44e3a8a
seate <sa20207@naver.com>
2025-05-06 23:52:11 +0900

    feat: ProductRepository 구현

---
+++ src/main/java/com/project/yogerOrder/product/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.project.yogerOrder.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.yogerOrder.product.entity.ProductEntity;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+}
