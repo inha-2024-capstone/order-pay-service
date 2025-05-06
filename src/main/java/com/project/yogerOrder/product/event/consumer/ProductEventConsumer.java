2ede922528cc2d67f80e2eab2845270fb34a33d9
seate <sa20207@naver.com>
2025-05-06 23:58:47 +0900

    feat: ProductEventConsumer 구현

---
+++ src/main/java/com/project/yogerOrder/product/event/consumer/ProductEventConsumer.java
@@ -0,0 +1,33 @@
+package com.project.yogerOrder.product.event.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+import com.project.yogerOrder.global.config.KafkaConfig;
+import com.project.yogerOrder.product.config.ProductTopic;
+import com.project.yogerOrder.product.dto.request.UpsertProductRequestDTO;
+import com.project.yogerOrder.product.event.ProductUpdatedEvent;
+import com.project.yogerOrder.product.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProductEventConsumer {
+
+	private final ProductService productService;
+
+	@KafkaListener(topics = ProductTopic.UPDATED, groupId = KafkaConfig.PRODUCT_GROUP,
+		containerFactory = KafkaConfig.KafkaConsumerConfig.PRODUCT_UPDATED_FACTORY)
+	public void productDeductionCompleted(ProductUpdatedEvent event, Acknowledgment acknowledgment) {
+		productService.upsertProduct(new UpsertProductRequestDTO(
+			event.productId(),
+			event.data().name(),
+			event.data().stock(),
+			event.data().price()
+		));
+
+		acknowledgment.acknowledge();
+	}
+}
