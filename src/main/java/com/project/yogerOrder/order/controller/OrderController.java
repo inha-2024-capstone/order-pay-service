fbecfe7d355ff1a3e4b2bc5d7ee9fabe47b5609c
seate <sa20207@naver.com>
2025-05-10 01:12:57 +0900

    refactor!: OrderController 복수 상품 대응하도록 변경

--- src/main/java/com/project/yogerOrder/order/controller/OrderController.java
+++ src/main/java/com/project/yogerOrder/order/controller/OrderController.java
@@ -1,16 +1,21 @@
 package com.project.yogerOrder.order.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.project.yogerOrder.order.dto.request.OrderRequestDTO;
-import com.project.yogerOrder.order.dto.request.OrdersCountRequestDTO;
-import com.project.yogerOrder.order.dto.response.OrderCountResponseDTOs;
 import com.project.yogerOrder.order.dto.response.OrderResponseDTOs;
 import com.project.yogerOrder.order.dto.response.OrderResultResponseDTO;
 import com.project.yogerOrder.order.service.OrderService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,18 +24,12 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/products/{productId}")
+    @PostMapping
     public ResponseEntity<OrderResultResponseDTO> orderProduct(@RequestHeader("User-Id") Long userId,
-                                                               @PathVariable("productId") Long productId,
                                                                @RequestBody @Valid OrderRequestDTO orderRequestDTO) {
-        Long orderId = orderService.orderProduct(userId, productId, orderRequestDTO);
+        String orderId = orderService.orderProduct(userId, orderRequestDTO);
 
         return new ResponseEntity<>(new OrderResultResponseDTO(orderId), HttpStatus.CREATED);
-    }
-
-    @PostMapping("/products/count")
-    public ResponseEntity<OrderCountResponseDTOs> countOrderByProductId(@RequestBody @Valid OrdersCountRequestDTO ordersCountRequestDTO) {
-        return new ResponseEntity<>(orderService.countOrdersByProductIds(ordersCountRequestDTO), HttpStatus.OK);
     }
 
     @GetMapping
