f26d672e22ebb0a1cc7d78336fe865924acf6753
seate <sa20207@naver.com>
2025-05-10 02:12:42 +0900

    refactor!: product event가 복수 상품 대응하도록 변경

--- src/main/java/com/project/yogerOrder/product/event/ProductUpdatedEvent.java
+++ src/main/java/com/project/yogerOrder/product/event/ProductUpdatedEvent.java
@@ -1,6 +1,7 @@
 package com.project.yogerOrder.product.event;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +9,16 @@ import jakarta.validation.constraints.NotNull;
 public record ProductUpdatedEvent(@NotNull Long productId, @NotBlank String eventId, @NotNull ProductEventType eventType,
 								  @NotNull ProductUpdatedData data, @NotNull LocalDateTime occurrenceDateTime) {
 
-    public record ProductUpdatedData(@NotBlank String name, @NotNull Integer stock, @NotNull Integer price) {
+	public static ProductUpdatedEvent of(Long productId, String name, Integer stock, Integer price) {
+		return new ProductUpdatedEvent(
+				productId,
+				UUID.randomUUID().toString(),
+				ProductEventType.UPDATED,
+				new ProductUpdatedData(name, stock, price),
+				LocalDateTime.now()
+		);
+	}
+
+    private record ProductUpdatedData(@NotBlank String name, @NotNull Integer stock, @NotNull Integer price) {
     }
 }
