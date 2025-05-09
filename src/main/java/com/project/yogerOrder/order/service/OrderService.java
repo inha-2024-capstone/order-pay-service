0e551852a3dc8fbf544fc57a8703123f67a02c63
seate <sa20207@naver.com>
2025-05-10 01:12:22 +0900

    refactor!: OrderService 복수 상품 대응하도록 변경

--- src/main/java/com/project/yogerOrder/order/service/OrderService.java
+++ src/main/java/com/project/yogerOrder/order/service/OrderService.java
@@ -1,22 +1,23 @@
 package com.project.yogerOrder.order.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
+import com.project.yogerOrder.global.config.MongoDBConfig;
 import com.project.yogerOrder.global.util.lock.OptimisticLockRetry;
 import com.project.yogerOrder.order.config.OrderConfig;
+import com.project.yogerOrder.order.dto.request.OrderItemRequestDTO;
 import com.project.yogerOrder.order.dto.request.OrderRequestDTO;
-import com.project.yogerOrder.order.dto.request.OrdersCountRequestDTO;
-import com.project.yogerOrder.order.dto.response.OrderCountResponseDTO;
-import com.project.yogerOrder.order.dto.response.OrderCountResponseDTOs;
 import com.project.yogerOrder.order.dto.response.OrderResponseDTOs;
 import com.project.yogerOrder.order.entity.OrderEntity;
+import com.project.yogerOrder.order.entity.OrderItem;
 import com.project.yogerOrder.order.entity.OrderState;
 import com.project.yogerOrder.order.event.producer.OrderEventProducer;
 import com.project.yogerOrder.order.exception.OrderNotFoundException;
@@ -24,6 +25,9 @@ import com.project.yogerOrder.order.repository.OrderRepository;
 import com.project.yogerOrder.order.util.duplicate.exception.OrderDuplicatedException;
 import com.project.yogerOrder.order.util.duplicate.service.OrderDuplicateCheckService;
 import com.project.yogerOrder.order.util.stateMachine.OrderStateChangeEvent;
+import com.project.yogerOrder.product.dto.response.ProductResponseDTO;
+import com.project.yogerOrder.product.exception.ProductNotFoundException;
+import com.project.yogerOrder.product.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,10 +45,12 @@ public class OrderService {
 
     private final OrderDuplicateCheckService orderDuplicateCheckService;
 
+    private final ProductService productService;
+
 
     // CREATE
-    @Transactional
-    public Long orderProduct(Long userId, Long productId, OrderRequestDTO orderRequestDTO) {
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER)
+    public String orderProduct(Long userId, OrderRequestDTO orderRequestDTO) {
         try {
             orderDuplicateCheckService.register(orderRequestDTO.orderRequestId());
         } catch (OrderDuplicatedException e) {
@@ -52,7 +58,10 @@ public class OrderService {
             throw e;
         }
 
-        OrderEntity pendingOrder = OrderEntity.createPendingOrder(productId, orderRequestDTO.quantity(), userId);
+        List<OrderItem> orderItems = orderRequestDTO.orderItems().stream().map(OrderItemRequestDTO::toOrderItem).toList();
+
+        Integer totalPrice = calculateTotalPrice(orderItems);
+        OrderEntity pendingOrder = OrderEntity.createPendingOrder(orderItems, totalPrice, userId);
         OrderEntity orderEntity = orderRepository.save(pendingOrder);
 
         orderEventProducer.publishOrderCreatedEvent(orderEntity);
@@ -60,9 +69,23 @@ public class OrderService {
         return orderEntity.getId();
     }
 
+    private Integer calculateTotalPrice(List<OrderItem> orderItems) {
+        Map<Long, Integer> idToPrice = productService.findByIds(orderItems.stream().map(OrderItem::productId).toList())
+            .stream().collect(Collectors.toMap(ProductResponseDTO::id, ProductResponseDTO::price));
+
+        return orderItems.stream().mapToInt(item -> {
+            Integer price = idToPrice.get(item.productId());
+            if (price == null) {
+                throw new ProductNotFoundException();
+            }
+
+            return price * item.quantity();
+        }).sum();
+    }
+
 
     // READ
-    public OrderEntity findById(Long orderId) {
+    public OrderEntity findById(String orderId) {
         return orderRepository.findById(orderId).orElseThrow(OrderNotFoundException::new);
     }
 
@@ -70,27 +93,15 @@ public class OrderService {
         return orderEntity.isPayable(config.validTime());
     }
 
-    @Transactional(readOnly = true)
-    public OrderCountResponseDTOs countOrdersByProductIds(OrdersCountRequestDTO ordersCountRequestDTO) {
-        List<OrderCountResponseDTO> orderCountResponseDTOS = ordersCountRequestDTO.productIds().stream().map(productId ->
-                new OrderCountResponseDTO(
-                        productId,
-                        orderRepository.countAllByProductIdAndState(productId, OrderState.COMPLETED)
-                )
-        ).toList();
-
-        return new OrderCountResponseDTOs(orderCountResponseDTOS);
-    }
-
-    @Transactional(readOnly = true)
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER, readOnly = true)
     public OrderResponseDTOs findApprovedOrdersByUserId(Long userId) {
         return OrderResponseDTOs.from(orderRepository.findAllByBuyerIdAndState(userId, OrderState.COMPLETED));
     }
 
     // UPDATE
     @OptimisticLockRetry
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void updateByDeductionSuccess(Long orderId) {
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER)
+    public void updateByDeductionSuccess(String orderId) {
         OrderEntity orderEntity = findById(orderId);
 
         if (orderEntity.getState() == OrderState.CANCELED) {
@@ -102,14 +113,14 @@ public class OrderService {
     }
 
     @OptimisticLockRetry
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void updateByDeductionFail(Long orderId) {
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER)
+    public void updateByDeductionFail(String orderId) {
         updateByStateChange(findById(orderId), OrderStateChangeEvent.STOCK_DEDUCT_FAILED);
     }
 
     @OptimisticLockRetry
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void updateByPaymentCompleted(Long orderId) {
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER)
+    public void updateByPaymentCompleted(String orderId) {
         OrderEntity orderEntity = findById(orderId);
 
         if (orderEntity.getState() == OrderState.CANCELED) {
@@ -121,14 +132,14 @@ public class OrderService {
     }
 
     @OptimisticLockRetry
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void updateByPaymentCanceled(Long orderId) {
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER)
+    public void updateByPaymentCanceled(String orderId) {
         updateByStateChange(findById(orderId), OrderStateChangeEvent.PAYMENT_CANCELED);
     }
 
     // 주기적 pending 상태 order를 만료 상태로 변경하고 상품 재고 release
     @Scheduled(cron = "${order.cron.expiration}")
-    @Transactional(isolation = Isolation.READ_COMMITTED)
+    @Transactional(transactionManager = MongoDBConfig.MONGO_TRANSACTION_MANAGER)
     @SchedulerLock(name = "orderExpirationSchedule", lockAtMostFor = "PT50S", lockAtLeastFor = "PT40S")
     public void orderExpirationSchedule() {
         OrderState.getPayableStates().forEach(orderState ->
