50415d635d206df7f99d0728c174ca9503c8099c
seate <sa20207@naver.com>
2025-05-10 02:36:41 +0900

    refactor: OrderCompletedEvent에서 DeleteProductFromCartRequestDTO 변환 과정 수정

--- src/main/java/com/project/yogerOrder/cart/event/consumer/CartEventConsumer.java
+++ src/main/java/com/project/yogerOrder/cart/event/consumer/CartEventConsumer.java
@@ -1,7 +1,5 @@
 package com.project.yogerOrder.cart.event.consumer;
 
-import java.util.Map;
-
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -10,6 +8,7 @@ import com.project.yogerOrder.cart.dto.request.DeleteProductFromCartRequestDTO;
 import com.project.yogerOrder.cart.service.CartService;
 import com.project.yogerOrder.global.config.KafkaConfig;
 import com.project.yogerOrder.order.config.OrderTopic;
+import com.project.yogerOrder.order.dto.request.OrderItemRequestDTO;
 import com.project.yogerOrder.order.event.OrderCompletedEvent;
 
 import lombok.RequiredArgsConstructor;
@@ -25,7 +24,7 @@ public class CartEventConsumer {
 		containerFactory = KafkaConfig.KafkaConsumerConfig.ORDER_COMPLETED_FACTORY)
 	public void orderCompleted(OrderCompletedEvent event, Acknowledgment acknowledgment) {
 		DeleteProductFromCartRequestDTO requestDTO = new DeleteProductFromCartRequestDTO(
-			event.getProducts().stream().map(Map.Entry::getKey).toList()
+			event.getOrderItems().stream().map(OrderItemRequestDTO::productId).toList()
 		);
 		cartService.deleteItems(event.getUserId(), requestDTO);
 
