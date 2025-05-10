df307d2853d780cfac63b18999065203036350ba
seate <sa20207@naver.com>
2025-05-10 11:36:57 +0900

    refactor: 불필요 외부 Product server 설정 삭제

--- src/main/java/com/project/yogerOrder/product/config/ProductConfig.java
+++
@@ -1,8 +0,0 @@
-package com.project.yogerOrder.product.config;
-
-import jakarta.validation.constraints.NotBlank;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "product")
-public record ProductConfig(@NotBlank String url) {
-}
