33ba1a1162354907c65e9f59baea1d8bdef9a52d
seate <sa20207@naver.com>
2025-10-02 19:13:56 +0900

    feat: Product reserve 기능 구현

--- src/main/java/com/project/yogerOrder/product/service/ProductService.java
+++ src/main/java/com/project/yogerOrder/product/service/ProductService.java
@@ -2,11 +2,16 @@ package com.project.yogerOrder.product.service;
 
 import java.util.List;
 
+import com.project.yogerOrder.order.entity.OrderItem;
 import com.project.yogerOrder.product.dto.request.UpsertProductRequestDTO;
 import com.project.yogerOrder.product.dto.response.ProductResponseDTO;
+import com.project.yogerOrder.product.exception.ProductInsufficientException;
 import com.project.yogerOrder.product.exception.ProductNotFoundException;
+import com.project.yogerOrder.product.exception.ProductServerStateException;
 
 public interface ProductService {
+    
+    void reserveStocks(String orderId, List<OrderItem> orderItems) throws ProductServerStateException, ProductNotFoundException, ProductInsufficientException;
 
     void upsertProduct(UpsertProductRequestDTO upsertProductRequestDTO);
 
