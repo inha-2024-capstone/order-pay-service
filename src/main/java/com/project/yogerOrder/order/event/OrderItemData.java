6c3f404ef0e2383d3332d41a2ee4ac246afc4556
seate <sa20207@naver.com>
2025-10-02 13:47:54 +0900

    refactor: OrderItemData 불필요 의존성 메서드 제거

--- src/main/java/com/project/yogerOrder/order/event/OrderItemData.java
+++ src/main/java/com/project/yogerOrder/order/event/OrderItemData.java
@@ -1,6 +1,5 @@
 package com.project.yogerOrder.order.event;
 
-import com.project.yogerOrder.order.dto.request.OrderItemRequestDTO;
 import com.project.yogerOrder.order.entity.OrderItem;
 
 import jakarta.validation.constraints.NotNull;
@@ -10,9 +9,5 @@ public record OrderItemData(@NotNull Long productId, @NotNull Integer quantity) 
 	public static OrderItemData from(OrderItem orderItem) {
 		return new OrderItemData(orderItem.productId(), orderItem.quantity());
 	}
-
-
-	OrderItemRequestDTO toDTO() {
-		return new OrderItemRequestDTO(this.productId, this.quantity);
-	}
+	
 }
