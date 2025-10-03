42ad7a334ccf925f1e11ed081212fe52b95b6a4b
seate <sa20207@naver.com>
2025-10-03 21:40:04 +0900

    feat: confirm product reservation event 구현

---
+++ src/main/java/com/project/yogerOrder/product/event/ConfirmProductReservationEvent.java
@@ -0,0 +1,39 @@
+package com.project.yogerOrder.product.event;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import com.project.yogerOrder.order.entity.OrderEntity;
+import com.project.yogerOrder.order.entity.OrderItem;
+import com.project.yogerOrder.order.event.OrderItemData;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record ConfirmProductReservationEvent(@NotNull String orderId, @NotBlank String eventId, @NotBlank ProductEventType eventType,
+                                             @NotNull ConfirmProductReservationData data, @NotNull LocalDateTime occurrenceDateTime) {
+
+    public static ConfirmProductReservationEvent from(OrderEntity orderEntity) {
+        return new ConfirmProductReservationEvent(
+                orderEntity.getId(),
+                UUID.randomUUID().toString(),
+                ProductEventType.CONFIRM_RESERVATION,
+                ConfirmProductReservationData.of(orderEntity.getBuyerId(), orderEntity.getOrderItems()),
+                LocalDateTime.now()
+        );
+    }
+
+    private record ConfirmProductReservationData(@NotNull Long userId, @NotEmpty List<OrderItemData> orderItems) {
+
+        private static ConfirmProductReservationData of(Long userId, List<OrderItem> orderItems) {
+            return new ConfirmProductReservationData(
+                userId,
+                orderItems.stream().map(OrderItemData::from).collect(Collectors.toList())
+            );
+        }
+
+    }
+}
--- src/main/java/com/project/yogerOrder/product/event/ProductEventType.java
+++ src/main/java/com/project/yogerOrder/product/event/ProductEventType.java
@@ -6,4 +6,9 @@ public enum ProductEventType {
     @JsonProperty("deductionCompleted") DEDUCTION_COMPLETED,
     @JsonProperty("deductionFailed") DEDUCTION_FAILED,
     @JsonProperty("updated") UPDATED,
+    @JsonProperty("confirm_reservation") CONFIRM_RESERVATION;
+    
+    public String toString() {
+        return this.name().toUpperCase();
+    }
 }
