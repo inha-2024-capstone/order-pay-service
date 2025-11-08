7a27b43f815c2083849ad26a48ba2340a11dbd4d
seate <sa20207@naver.com>
2025-11-08 13:59:34 +0900

    test: kafka ConfirmReservation testConfig 구현

--- src/test/java/com/project/yogerOrder/order/OrderIntegrationTest.java
+++ src/test/java/com/project/yogerOrder/order/OrderIntegrationTest.java
@@ -4,10 +4,16 @@ import static org.awaitility.Awaitility.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Stream;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,8 +22,16 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.project.yogerOrder.global.UsingTestContainerTest;
@@ -27,6 +41,8 @@ import com.project.yogerOrder.order.dto.request.OrderRequestDTO;
 import com.project.yogerOrder.order.entity.OrderEntity;
 import com.project.yogerOrder.order.entity.OrderItem;
 import com.project.yogerOrder.order.entity.OrderState;
+import com.project.yogerOrder.order.event.OrderCompletedEvent;
+import com.project.yogerOrder.order.event.config.OrderTopic;
 import com.project.yogerOrder.order.repository.OrderRepository;
 import com.project.yogerOrder.order.util.duplicate.exception.OrderDuplicatedException;
 import com.project.yogerOrder.payment.entity.PaymentEntity;
@@ -34,6 +50,7 @@ import com.project.yogerOrder.payment.event.PaymentCanceledEvent;
 import com.project.yogerOrder.payment.event.PaymentCompletedEvent;
 import com.project.yogerOrder.payment.event.config.PaymentTopic;
 import com.project.yogerOrder.product.dto.response.ProductResponseDTO;
+import com.project.yogerOrder.product.event.ConfirmProductReservationEvent;
 import com.project.yogerOrder.product.event.ProductDeductionCompletedEvent;
 import com.project.yogerOrder.product.event.ProductDeductionFailedEvent;
 import com.project.yogerOrder.product.event.config.ProductTopic;
@@ -260,5 +277,35 @@ public class OrderIntegrationTest extends UsingTestContainerTest {
         // then
         Assertions.assertThrows(OrderDuplicatedException.class, () -> orderController.orderProduct(userId, orderRequestDTO));
     }
-
+    
+    
+    @TestConfiguration
+    public static class TestKafkaConfig {
+        
+        private static final String CONFIRM_RESERVATION_FACTORY = "confirmReservationFactory";
+        
+        private HashMap<String, Object> consumerConfig() {
+            HashMap<String, Object> config = new HashMap<>();
+            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, getKafkaBootstrapServers());
+            config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+            
+            return config;
+        }
+        
+        @Bean(CONFIRM_RESERVATION_FACTORY)
+        public ConcurrentKafkaListenerContainerFactory<String, ConfirmProductReservationEvent> confirmProductReservationEventConcurrentKafkaListenerContainerFactory() {
+            ConcurrentKafkaListenerContainerFactory<String, ConfirmProductReservationEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+            
+            DefaultKafkaConsumerFactory<String, ConfirmProductReservationEvent> consumerFactory = new DefaultKafkaConsumerFactory<>(
+                consumerConfig(),
+                new StringDeserializer(),
+                new JsonDeserializer<>(ConfirmProductReservationEvent.class, false)
+            );
+            
+            factory.setConsumerFactory(consumerFactory);
+            factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+            return factory;
+        }
+    }
+    
 }
